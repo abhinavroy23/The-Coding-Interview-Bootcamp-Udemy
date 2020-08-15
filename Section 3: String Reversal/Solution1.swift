8cafcad5b52e72c407e66a73bbb1fd8685563997
Abhinav Roy <abhinav.roy23@gmail.com>
2020-08-15 18:52:12 +0530

    Update Solution1.swift

--- Section 3: String Reversal/Solution1.swift
+++ Section 3: String Reversal/Solution1.swift
@@ -1,1 +1,13 @@
 
+/*
+
+Solution 1:
+1. Convert input string to Array
+2. reverse the array
+3. cast it back to String
+
+*/
+
+func reverse(_ input : String) -> String {
+  return String(input.reversed())
+}
