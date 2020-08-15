294f45daa9704f93c4c7a7cd0800dd9ed3874b37
Abhinav Roy <abhinav.roy23@gmail.com>
2020-08-15 18:56:39 +0530

    Create Solution2.swift

---
+++ Section 3: String Reversal/Solution2.swift
@@ -0,0 +1,18 @@
+
+/*
+
+Solution 2:
+1. Create an empty variable
+2. loop on input string
+3. variable = currentChar + variable
+4. return variable
+
+*/
+
+func reverse(_ input : String) -> String {
+  var result : String = ""
+  for char in input {
+    result = String(char) + result
+  }
+  return result
+}
